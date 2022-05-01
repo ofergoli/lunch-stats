4c3a0c7d4e4887a193220568f7fa65d47fca1a40
Ofer Golibroda <ofer.golibroda@forter.com>
2022-05-01 16:21:01 +0300

    median-box

--- index.js
+++ index.js
@@ -1,18 +1,73 @@
-function debounce(func, delay) {
-  let timer = null;
-  return (...args) => {
-    clearTimeout(timer);
-    timer = setTimeout(() => func(...args), delay);
+$(document).ready(function () {
+  $('.js-example-basic-multiple').select2();
+});
+
+$('#restaurant-dropdown').on('select2:close', function () {
+  const selectedValues = $('#restaurant-dropdown').select2('data');
+  const values = selectedValues.map(({ text }) => text);
+  clearMedianBox();
+  for(let value of values) {
+    createMedianByRestaurant(ALL_REPORTS, value);
+  }
+  search(values);
+});
+
+function uniq(arr) {
+  return [...new Set(arr)].sort();
+}
+
+function search(patterns) {
+  const traces = patterns.map(pattern =>
+      reportsToTrace(
+          ALL_REPORTS.filter(report => report.resturaunts.toLowerCase().includes(pattern.toLowerCase())),
+          pattern,
+      ),
+  );
+  Plotly.react(CHART, traces, LAYOUT, CONFIG);
+}
+
+function createDropDown(values) {
+  const dropdown = document.getElementById('restaurant-dropdown');
+  for (let value of values) {
+    const opt = document.createElement('option');
+    opt.value = value;
+    opt.innerText = value;
+    dropdown.appendChild(opt);
   }
 }
-const onSearch = debounce((ev) => {
-  const patterns = ev.target.value.split(',').map(part => part.trim()).filter(pattern => pattern);
-  if (patterns.length === 0) {
-    patterns.push('');
+
+function clearMedianBox() {
+  let list = document.getElementById("median-box");
+  while (list.hasChildNodes()) {
+    list.removeChild(list.firstChild);
   }
-  const traces = patterns.map(pattern => reportsToTrace(ALL_REPORTS.filter(report => report.resturaunts.toLowerCase().includes(pattern.toLowerCase())), pattern));
-  Plotly.react(CHART, traces, LAYOUT, CONFIG);
-}, 300);
+}
+
+function createMedianBox(date, restaurant) {
+  const medianBox = document.createElement('div');
+  const text = document.createElement('h4');
+  text.innerText = `מסעדת ${restaurant} מגיע עד ${date.getHours() + ':' + `${(date.getMinutes() + '').length === 1 ? `0${date.getMinutes()}` : date.getMinutes()}`}`;
+  medianBox.appendChild(text);
+  medianBox.id = restaurant;
+  document.getElementById('median-box').appendChild(medianBox);
+}
+
+function median(numbers) {
+  const sorted = Array.from(numbers).sort((a, b) => a - b);
+  const middle = Math.floor(sorted.length / 2);
+
+  if (sorted.length % 2 === 0) {
+    return (sorted[middle - 1] + sorted[middle]) / 2;
+  }
+
+  return sorted[middle];
+}
+
+function createMedianByRestaurant(reports, restaurant) {
+  const values = reports.filter(r =>r.resturaunts === restaurant).map(d=> new Date(`01-01-2022,${d.time}`).getTime());
+  const d = new Date(median(values));
+  createMedianBox(d, restaurant);
+}
 
 function reportsToTrace(reports, name) {
   return {
@@ -23,11 +78,21 @@ function reportsToTrace(reports, name) {
     x: reports.map(report => report.date),
     y: reports.map(report => '1970-01-01 ' + report.time),
   };
-};
+}
 const CHART = document.getElementById('chart');
-const LAYOUT = { title: 'מתי דיווחו על הגעה?', showlegend: true, yaxis: { range: ['1970-01-01 11:00:00', '1970-01-01 15:00:00'], tickformat: '%H:%M' } };
+const LAYOUT = {
+  title: 'מתי דיווחו על הגעה?',
+  showlegend: true,
+  yaxis: { range: ['1970-01-01 11:00:00', '1970-01-01 15:00:00'], tickformat: '%H:%M' },
+};
 const CONFIG = { scrollZoom: true };
-const patternInput = document.getElementById('pattern');
-patternInput.addEventListener('change', ev => onSearch(ev));
-patternInput.addEventListener('keypress', ev => onSearch(ev));
+
+createDropDown(
+    uniq(
+        ALL_REPORTS
+            .map(report => report.resturaunts.split(/\s+/))
+            .flat()
+            .map(word => word.trim()),
+    ),
+);
 Plotly.newPlot(CHART, [reportsToTrace(ALL_REPORTS)], LAYOUT, CONFIG);
